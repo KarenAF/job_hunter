ad678bf1295deae91b82967a8d67d8ecd4f9f9c6
kareningenuity <karenjxliu@hotmail.com>
2016-08-21 18:00:48 -0500

    i have no idea what is going on. cant create a new jobskill.

--- app/controllers/job_skills_controller.rb
+++ app/controllers/job_skills_controller.rb
@@ -4,7 +4,12 @@ class JobSkillsController < ApplicationController
   end
 
   def create
+    @job = Job.find_by(id: params['job_id'])
     jobskill = JobSkill.create(
-      user_id:
+      user_id: current_user.id,
+      skill_id: params[:skill_id],
+      job_id: @job.id
     )
+    redirect_to 'jobs/#{@job.id}'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  get 'jobs/:job_id/job_skills/new' => 'job_skills#new'  
-  post 'jobs/:job_id' => 'job_skills#create'  
+  get 'job_skills/:job_id/new' => 'job_skills#new'  
+  post 'job_skills/:id' => 'job_skills#create'  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
