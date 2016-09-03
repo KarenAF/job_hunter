3d93f699e046fe998f6d0b2156c31a40f151f162
kareningenuity <karenjxliu@hotmail.com>
2016-09-03 18:51:43 -0500

    added create method in networkings controller

--- app/controllers/networkings_controller.rb
+++ app/controllers/networkings_controller.rb
@@ -8,4 +8,21 @@ class NetworkingsController < ApplicationController
     @connections = current_user.networkings
     render 'new.html.erb'
   end
+
+  def create
+    @networking = Networking.new(
+      user_id: current_user.id,
+      first_name: params['first_name'],
+      last_name: params['last_name'],
+      date_contacted: params['date_contacted'],
+      email: params['email'],
+      job_id: params['job_id'],
+      emailed: params['emailed'],
+      phoned: params['phoned'],
+      letter_sent: params['letter_sent']
+    )
+    @networking.save
+    flash[:success] = "networking connection successfully saved"
+    redirect_to '/networkings'  
+  end  
 end
