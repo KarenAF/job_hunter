d111d8e5e4c0a2ed77682b1e9bbe8c6e32e78b24
kareningenuity <karenjxliu@hotmail.com>
2016-09-08 20:55:12 -0500

    networkings: added friendly contacted method to show yes or no for contacted or not

--- app/models/networking.rb
+++ app/models/networking.rb
@@ -1,4 +1,27 @@
 class Networking < ActiveRecord::Base
   belongs_to :job 
   belongs_to :user
+
+
+  def friendly_emailed
+    if emailed == true
+      return "yes"
+    else
+      return "no"
+    end
+  end
+  def friendly_phoned
+    if phoned == true
+      return "yes"
+    else
+      return "no"
+    end
+  end
+  def friendly_lettered
+    if letter_sent == true
+      return "yes"
+    else
+      return "no"
+    end
+  end    
 end
