482fe5bdabf53a4b3529bd386758a01ec0be9747
kareningenuity <karenjxliu@hotmail.com>
2016-09-09 23:04:15 -0500

    not sure what i am doing right now, but i know i a want to get the api for my data all set up

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::JobsController < ApplicationController
   def index
+    @job = Job.find_by(id: params[:job_id])
+    all_jobs = Skill.find_by(job_id: @job.id)
     render 'index.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
   namespace :v1 do
-    get '/api/v1/jobs' => 'jobs#index'
+    get 'jobs' => 'jobs#index'
     end
   end
 
