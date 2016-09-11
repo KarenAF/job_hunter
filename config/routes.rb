a0b96c19809879759b797bcdc6771fd44d3f38a9
kareningenuity <karenjxliu@hotmail.com>
2016-09-11 16:31:32 -0500

    tried to add dropdown to repeated index jobs grid, but it didnt work. created api networking controller to start building angular on the networkings controller page, so tht i can try the dropdown menu on there

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
   namespace :v1 do
+    get 'networkings/index'
+    end
+  end
+
+  namespace :api do
+  namespace :v1 do
     get '/jobs' => 'jobs#index'
     post '/jobs' => 'jobs#create'
     end
