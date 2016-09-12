6d613d65e7d7dda7bbf6a35ab906433f608514c2
kareningenuity <karenjxliu@hotmail.com>
2016-09-12 12:14:11 -0500

    figured out the order of code in app.js that wont break the code

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,11 @@
 (function() {
   "use strict";
 
-  // var angularMaterialize = require('angular-materialize');
-  // angular.module('app', [angularMaterialize]);
   angular.module("app", ["ngAnimate"]);
 // 9/12/16 Added angularmaterialize. it was commented out before.
+
+  var angularMaterialize = require('angular-materialize');
+  angular.module('app', [angularMaterialize]);
+
 }());
 
