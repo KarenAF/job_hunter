b93922ddd5528794be2e484980c64c481316d6af
kareningenuity <karenjxliu@hotmail.com>
2016-09-12 12:09:39 -0500

    reverted back to previous state before the last commit because it broke the app.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,8 @@
 (function() {
   "use strict";
 
-  var angularMaterialize = require('angular-materialize');
-  angular.module('app', [angularMaterialize]);
+  // var angularMaterialize = require('angular-materialize');
+  // angular.module('app', [angularMaterialize]);
   angular.module("app", ["ngAnimate"]);
 // 9/12/16 Added angularmaterialize. it was commented out before.
 }());
