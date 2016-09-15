121464ec577ad01288bdd55dfa3ba35b243f1630
kareningenuity <karenjxliu@hotmail.com>
2016-09-14 19:52:40 -0500

    deleted extraneous commented-out code in networkingsctrl.js

--- app/assets/javascripts/networkingsCtrl.js
+++ app/assets/javascripts/networkingsCtrl.js
@@ -13,17 +13,4 @@
     window.$scope = $scope;
   });
 
-
-  // angular.module("app").controller("networkingsCtrl", function($scope, $http, $window) {
-  //   $scope.setup = function() {
-  //     $http.get('/api/v1/networkings.json').then(function(response){
-  //       $scope.networkings = response.data;
-
-  //   window.$scope = $scope;
-   
-  //   $scope.message = "Hello world";
-  // });
-  //   });
-
-
 })();
